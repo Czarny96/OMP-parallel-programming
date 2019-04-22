d8827fab8bc92afea7195e23e384fba8f879e28b
Michał Łukomski <michallukomskiit@gmail.com>
2019-04-22 22:50:48 +0200

    [DEV] Added pragma omp and time survey

--- Double-integral/Main.cpp
+++ Double-integral/Main.cpp
@@ -48,10 +48,14 @@ int main()
 	// Time of calculate integral without omp: 2.4992
 	auto timeCalculateIntegralStart = chrono::high_resolution_clock::now();
 
+	// Time of calculate integral: 0.566551
+#pragma omp parallel for private(x, y, innerIntegralResult) reduction(+:result)
 	for (y = 1; y <= outer.numberOfRectangles; ++y)
 	{
 		innerIntegralResult = 0.0;
 
+// Time of calculate integral: 0.893089
+// #pragma omp parallel for private(x) reduction(+:innerIntegralResult)
 		for (x = 1; x <= inner.numberOfRectangles; ++x)
 		{
 			innerIntegralResult += Pattern(inner.min + x * innerRectWidth, outer.min + y * outerRectWidth) * innerRectWidth;
