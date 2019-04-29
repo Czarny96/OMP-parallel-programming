6c1f1d8a3bb20b109362cea5d8bedfde515ae10e
Michał Łukomski <michallukomskiit@gmail.com>
2019-04-29 22:05:27 +0200

    [DEV] Added entropy calculation, pragma omp and time survey

--- Entropy/Main.cpp
+++ Entropy/Main.cpp
@@ -3,34 +3,64 @@
 #include <chrono>
 #include <random>
 #include <time.h>
+#include <math.h>
 
 using namespace std;
 
-/*
-Entropy pattern: S = ln(N! / (n1! * n2! * ... * n6!) )
-N - number of all cube
-n1 - number of all number 1 on cube
-n2 - number of all number 2 on cube
-.
-.
-.
-n6 - number of all number 6 on cube
-*/
-
-#define NUMBEROFALLCUBE 600
-#define NUMBEROFITERATIONS 10
+#define NUMBEROFALLCUBE 10000
+#define NUMBEROFITERATIONS 1000
+#define M_PI 3.14
 
 unsigned short cubeMatrix[NUMBEROFALLCUBE];
 unsigned short sumOfAllCube = 0;
 
+long double entropy;
+
+// Time of calculate without OMP in functions: 5.26161
+double StirlingPattern(int value)
+{
+	if (value != 0)
+	{
+		double firstPart = 0;
+		int i;
+ #pragma omp parallel for private(i) reduction(+ : firstPart)
+		for (i = 1; i <= value; i++)
+		{
+			firstPart += log(value / exp(1));
+		}
+
+		double secoundPart = log(sqrt(2 * value * M_PI));
+
+		return firstPart + secoundPart;
+	}
+
+	return 0;
+}
+
+long double EntropyCalculation(int n, int matrix[6])
+{
+	long double counter = StirlingPattern(n);
+	long double denominator = 0;
+	int i;
+
+#pragma omp parallel for private(i) reduction(+ : denominator)
+	for (i = 0; i < 6; i++)
+	{
+		denominator += StirlingPattern(matrix[i]);
+	}
+
+	return counter - denominator;
+}
+
 int main()
 {
 	cout << "Hello Entropy" << endl;
-
+	// Time of set up value on cube matrix without OMP: 3.279e-05
 	auto timeSetUpValueOnCubeMatrixStart = chrono::high_resolution_clock::now();
 
 	int i, j;
-
+	//Time of set up value on cube matrix: 0.00461313
+#pragma omp parallel for private(i) shared(cubeMatrix)
 	for (i = 0; i < NUMBEROFALLCUBE; i++)
 	{
 		cubeMatrix[i] = 1;
@@ -44,12 +74,17 @@ int main()
 	uniform_int_distribution<int> getRandomNumberOfCube(0, 5);
 	uniform_int_distribution<int> getRandomChanceToChangeNumberOnCube(1, 20);
 	
-	auto timeCalculateEntropyStart = chrono::high_resolution_clock::now();
+	// Time of calculate wtihout OMP: 7.83094
+	auto timeCalculateStart = chrono::high_resolution_clock::now();
 
+// Time of calculate: 2.31776
+#pragma omp parallel for private(i, j) reduction(+ : sumOfAllCube)
 	for (i = 0; i < NUMBEROFITERATIONS; i++)
 	{
-		unsigned short theSameValueOnCube[6] = { 0, 0, 0, 0, 0, 0 };
-
+		int theSameValueOnCube[6] = { 0, 0, 0, 0, 0, 0 };
+		sumOfAllCube = 0;
+// Time of calculate: 3.80669
+//#pragma omp parallel for private(j) reduction(+ : sumOfAllCube)
 		for (j = 0; j < NUMBEROFALLCUBE; j++)
 		{
 			if (getRandomChanceToChangeNumberOnCube(generateSeed) == 5)
@@ -58,28 +93,22 @@ int main()
 			}
 
 			theSameValueOnCube[cubeMatrix[j]] += 1;
+			sumOfAllCube++;
 		}
 
-		// TODO: CALCULATE ENTROPY
+		entropy = EntropyCalculation(NUMBEROFALLCUBE, theSameValueOnCube);
 
-
-		sumOfAllCube = 0;
-
-		for (j = 0; j < 6; j++)
-		{
-			sumOfAllCube += theSameValueOnCube[j];
-		}
-
+#pragma omp critical
 		cout << theSameValueOnCube[0] << ' ' << theSameValueOnCube[1] << ' ' << theSameValueOnCube[2] << ' '
-			<< theSameValueOnCube[3] << ' ' << theSameValueOnCube[4] << ' ' << theSameValueOnCube[5]  << " | " << sumOfAllCube << " | " << i << endl;
-
+			<< theSameValueOnCube[3] << ' ' << theSameValueOnCube[4] << ' ' << theSameValueOnCube[5]  << " | "
+			<< entropy << " | " << sumOfAllCube << " | " << i << endl;
 	}
 
-	auto timeCalculateEntropyFinish = chrono::high_resolution_clock::now();
-	chrono::duration<double> elapsedTimeOfCalculateEntropy = timeCalculateEntropyFinish - timeCalculateEntropyStart;
+	auto timeCalculateFinish = chrono::high_resolution_clock::now();
+	chrono::duration<double> elapsedTimeOfCalculate = timeCalculateFinish - timeCalculateStart;
 
 	cout << "Time of set up value on cube matrix: " << elapsedTimeOfSetUpValueOnCubeMatrix.count() << endl;
-	cout << "Time of calculate entropy: " << elapsedTimeOfCalculateEntropy.count() << endl;
+	cout << "Time of calculate: " << elapsedTimeOfCalculate.count() << endl;
 
 	getchar();
 	return 0;
